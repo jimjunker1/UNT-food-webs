32a975bbc5119ba38625ad0999881df0faa3f724
Jim Junker <james.junker1@gmail.com>
2024-10-08 14:34:26 -0500

    site x date data product summaries save objects

--- R/NEON_data-grab.R
+++ R/NEON_data-grab.R
@@ -52,9 +52,10 @@ site_date_plant_biomass_m2 = plant_biomass %>%
   summarise(arealAdjDryMass_gm2 = sum(arealAdjDryMass, na.rm = TRUE),
             arealAdjAshFreeDryMass_gm2 = sum(arealAdjAshFreeDryMass, na.rm = TRUE))
 
+saveRDS(site_date_plant_biomass_m2, here::here("data/derived-data/site_date_plant_biomass_m2.rds"))
 #### stream-date of microbial abundance
 
-#### stream-date water quality
+#### stream-date water quality ----
 # this is a list
 site_date_waq_lab = waq_lab %>%
   group_by(siteID, collectDate) %>%
@@ -72,7 +73,74 @@ site_date_waq_lab_summ = waq_lab %>%
               names_sep = "\n", values_from = analyteConcentration,
               values_fn = mean, values_fill = NA)
 
-#### site-date epilithon chem
+saveRDS(site_date_waq_lab_summ, here::here("data/derived-data/site_date_waq_lab_summ.rds"))
+#### site-date epilithon chem ----
+
+site_date_epi_chlorophyll = epi_field %>%
+  select(siteID, collectDate, namedLocation, sampleID = parentSampleID, benthicArea) %>%
+  filter( grepl("episammon|epiphyton", sampleID, ignore.case = TRUE)) %>%
+  mutate(collectDate = as.Date(collectDate)) %>%
+  left_join(epi_lab %>%
+              filter(grepl("chlorophyll", analysisType),
+                     grepl("episammon|epiphyton", sampleID, ignore.case = TRUE)) %>%
+              select(siteID, collectDate, namedLocation, sampleID,  fieldSampleVolume) %>% #analysisType
+              mutate(collectDate = as.Date(collectDate)) %>%
+              rowwise() %>%
+              mutate(sampleID = paste(sapply(strsplit(sampleID, "\\."), "[", 1:4), collapse = "."))) %>%
+  mutate(arealMult = fieldSampleVolume/benthicArea) %>%
+  left_join(epi_extlab %>%
+              filter(grepl("chlorophyll", sampleType),
+                     grepl("episammon|epiphyton", sampleID, ignore.case = TRUE)) %>%
+              select(siteID, collectDate, namedLocation, sampleID, percentFilterAnalyzed, analyte, analyteConcentration, plantAlgaeLabUnits) %>%
+              rowwise() %>%
+              mutate(sampleID = paste(sapply(strsplit(sampleID, "\\."), "[", 1:4), collapse = "."))) %>%
+  mutate(analyteArealMass = analyteConcentration * arealMult) %>%
+  mutate(analyteArealMass = case_when(grepl("micrograms.*", plantAlgaeLabUnits) ~ analyteArealMass/1000,
+                                      .default = analyteArealMass),
+         plantAlgaeLabUnits = case_when(grepl("micrograms.*", plantAlgaeLabUnits) ~ "milligrams_m2",
+                                        .default = plantAlgaeLabUnits)) %>%
+  select(siteID, collectDate, namedLocation, analyte, analyteArealMass, plantAlgaeLabUnits)
+
+saveRDS(site_date_epi_chlorophyll, here::here("data/derived-data/site_date_epi_chlorophyll.rds"))
+
+site_date_epi_chem = epi_field %>%
+  select(siteID, collectDate, namedLocation, sampleID = parentSampleID, benthicArea) %>%
+  filter( grepl("episammon|epiphyton", sampleID, ignore.case = TRUE)) %>%
+  mutate(collectDate = as.Date(collectDate)) %>%
+  left_join(epi_lab %>%
+              filter(!grepl("chlorophyll", analysisType),
+                     grepl("episammon|epiphyton", sampleID, ignore.case = TRUE)) %>%
+              select(siteID, collectDate, namedLocation, sampleID,  fieldSampleVolume) %>% #analysisType
+              mutate(collectDate = as.Date(collectDate)) %>%
+              rowwise() %>%
+              mutate(sampleID = paste(sapply(strsplit(sampleID, "\\."), "[", 1:4), collapse = "."))) %>%
+  mutate(arealMult = fieldSampleVolume/benthicArea) %>%
+  left_join(epi_extlab %>%
+              filter(!grepl("chlorophyll", sampleType),
+                     !grepl("perMill", plantAlgaeLabUnits),
+                     grepl("episammon|epiphyton", sampleID, ignore.case = TRUE)) %>%
+              select(siteID, collectDate, namedLocation, sampleID, percentFilterAnalyzed, analyte, analyteConcentration, plantAlgaeLabUnits) %>%
+              rowwise() %>%
+              mutate(sampleID = paste(sapply(strsplit(sampleID, "\\."), "[", 1:4), collapse = "."))) %>%
+  mutate(analyteArealMass = analyteConcentration * arealMult) %>%
+  mutate(analyteArealMass = case_when(grepl("micrograms.*", plantAlgaeLabUnits) ~ analyteArealMass/1e6,
+                                      .default = analyteArealMass),
+         plantAlgaeLabUnits = case_when(grepl("micrograms.*", plantAlgaeLabUnits) ~ "grams_m2",
+                                        .default = plantAlgaeLabUnits)) %>%
+  select(siteID, collectDate, namedLocation, analyte, analyteArealMass, plantAlgaeLabUnits) %>%
+  filter(!is.na(analyteArealMass)) %>%
+  pivot_wider(id_cols = c(siteID, collectDate),
+              names_from = c(analyte, plantAlgaeLabUnits),
+              names_sep = "\n", values_from = c(analyteArealMass),
+              values_fn = mean, values_fill = NA) %>%
+  mutate(`carbon\nmol_m2` = `carbon\ngrams_m2`/12.001,
+         `nitrogen\nmol_m2` = `nitrogen\ngrams_m2`/14.007,
+         `phosphorus\nmol_m2` = `phosphorus\ngrams_m2`/30.974,
+         `sulfur\nmol_m2` = `sulfur\ngrams_m2`/32.06) %>%
+  mutate(across(everything(), ~ifelse(.x == 0, NA_real_, .x)))
+
+saveRDS(site_date_epi_chem, here::here("data/derived-data/site_date_epi_chem.rds"))
+
 
 site_date_epi_field = epi_field %>%
   select(siteID, collectDate, namedLocation, sampleID = parentSampleID, benthicArea) %>%
@@ -97,11 +165,12 @@ site_date_seston = site_date_epi_field %>%
               values_fn = mean,
               values_fill = NA)
 
+saveRDS(site_date_seston, here::here("data/derived-data/site_date_seston.rds"))
 
 ## need to figure out why there is no volumeFiltered values ## on many of the samples
-site_date_epilithon = site_date_epi_field %>%
-  filter(grepl("EPILITHON", sampleID)) %>%
-  filter(!is.na(analyte))
+# site_date_epilithon = site_date_epi_field %>%
+#   filter(grepl("EPILITHON", sampleID)) %>%
+#   filter(!is.na(analyte))
 
 
 # site_date_epi_extlab = epi_extlab %>%
@@ -117,10 +186,45 @@ site_date_epilithon = site_date_epi_field %>%
 #               names_from = c(analyte, plantAlgaeLabUnits), names_sep = "\n",
 #               values_from = analyteConcentration,
 #               values_fn =mean, values_fill = NA)
-#### stream-date of riparian cover
+#### stream-date of riparian cover ----
 
 site_date_riparian = riparian_cov %>%
   filter(riparianSubsystem == "stream") %>%
-  select(siteID, namedLocation, startDate, riparianDominantVegetation, riparianWaterDepth, wettedWidth, riparianClass, riparianSubclass, riparianDominantVegetation, bigTreeType, bigTreeCoverClass, smallTreeType, smallTreeCoverClass, woodyShrubSaplingType, woodyShrubGroundCoverClass,)
+  select(siteID, namedLocation, startDate, riparianDominantVegetation,
+         riparianWaterDepth, wettedWidth, riparianClass, riparianSubclass,
+         riparianDominantVegetation, bigTreeType, bigTreeCoverClass,
+         smallTreeType, smallTreeCoverClass, woodyShrubSaplingType,
+         woodyShrubGroundCoverClass, shortHerbCoverClass, standingWaterCoverClass,
+         bareDirtCoverClass, buildingPresence, roadPresence, boatRampPresence,
+         lawnsParksPresence, agriculturePresence, industryPresence, pavementPresence,
+         trashPresence, dominantBankAngle, bankUndercut)
 
+saveRDS(site_date_riparian, here::here("data/derived-data/site_date_riparian.rds"))
+
+site_date_riparian_comp = riparian_comp %>%
+  select(siteID, startDate, measurementLocation, measurementDirection, totalDensiometerPoints,
+         coveredDensiometerPoints, canopyCoverPercent)
+
+saveRDS(site_date_riparian_comp, here::here("data/derived-data/site_date_riparian_comp.rds"))
+#### stream-date of sediment cover ----
+
+site_date_sed_lab = sed_lab %>%
+  filter(is.na(analyteConcText)) %>%
+  select(siteID, sampleID, collectDate = "startDate", analyte, analyteConcentration, analyteUnits) %>%
+  pivot_wider(id_cols = c(siteID, collectDate),
+              names_from = c(analyte, analyteUnits),
+              values_from = analyteConcentration,
+              values_fn = mean,
+              values_fill = NA)
+
+saveRDS(site_date_sed_lab, here::here("data/derived-data/site_date_sed_lab.rds"))
+# site_date_sed_text = sed_lab %>%
+#   filter(!is.na(analyteConcText)) %>%
+#   select(siteID, namedLocation, sampleID, startDate, analyte, analyteConcText) #%>%
+#
+# sed_fieldPoint %>% View()
+#
+# sed_fieldStation %>% View()
+
+#### site-date-water isotopes
 
